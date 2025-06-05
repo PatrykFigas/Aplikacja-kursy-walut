8751cbd0366da9dc82834441fb20914f1a3be3c7
Patryk Figas <patrykf09@wp.pl>
2025-06-06 01:01:09 +0200

    Skonfigurowano komponent Currency jako standalone i uruchomiono aplikację

---
+++ frontend/frontend-app/src/app/app.config.ts
@@ -0,0 +1,20 @@
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  provideBrowserGlobalErrorListeners
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+
+import { routes } from './app.routes';
+import { CurrencyComponent } from './components/currency/currency.component';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    CurrencyComponent
+  ]
+};
---
+++ frontend/frontend-app/src/app/components/currency/currency.module.ts
@@ -0,0 +1,4 @@
+import { importProvidersFrom } from '@angular/core';
+import { CurrencyComponent } from './currency.component';
+
+export const CurrencyModule = importProvidersFrom(CurrencyComponent);
